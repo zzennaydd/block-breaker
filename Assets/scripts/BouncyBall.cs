347b1a6fbfa75eb4cc5dff62afae1470f90ca2fb
zzenchery <zy.aydogan@gmail.com>
2025-02-05 21:17:31 +0300

    .

--- Assets/scripts/BouncyBall.cs
+++ Assets/scripts/BouncyBall.cs
@@ -6,18 +6,24 @@ public class BouncyBall : MonoBehaviour
 {
     [SerializeField] private float minY = -5.5f;
     private Rigidbody2D rb;
+    private float maxVelocity = 15f;
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if(transform.position.y < minY)
         {
             transform.position = Vector3.zero;
             rb.velocity = Vector3.zero;
+        }
+
+        if(rb.velocity.magnitude > maxVelocity)
+        {
+            rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxVelocity);
         }
         
     }
